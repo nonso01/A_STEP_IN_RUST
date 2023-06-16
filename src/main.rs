2c81d18b15accf202b2492537949e94ff95d92fe
nonso01 <chukwunonsomartin01@gmail.com>
2023-06-16 21:19:20 +0100

    enum and structs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-// constants, some might be definded already
-const PI: f64 = 3.142857;
-const ONE_POINT_THREE: f64 = 1.333333;
-
-// struct basics
-
+// enums vs structs, learning about enums and special Option
 #[derive(Debug)]
-struct Sphere {
-    radius: f64,
-   // width: f64,
-   // height: f64,
-}
-
-impl Sphere {
-    fn volume(&self) -> f64 {
-        ONE_POINT_THREE * PI * self.radius * self.radius * self.radius
-    }
-
-    fn area(&self) -> f64 {
-        PI * self.radius * self.radius
-    }
+enum Kingdom {
+    Monera(String),
+    Protoctista(String),
+    Fungi(String),
+    Plantae(String),
+    Animalia(String),
 }
 
 fn main() {
-    let sphere1 = Sphere {
-        radius: 4.5,
-       // height: 10.0,
-      //  width: 5.0,
-    };
-    
-    println!("the radius of sphere1 is {:?} and area is {:?}",
-             sphere1.volume(),
-             sphere1.area());
+    let human = Kingdom::Animalia(String::from("mammal"));
+    println!("human is {:?}", human);
 }
-
-
