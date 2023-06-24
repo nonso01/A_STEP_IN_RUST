1fb67891592545edbd510ac392fdfc493c058d58
nonso01 <chukwunonsomartin01@gmail.com>
2023-06-24 16:08:11 +0100

    more about match

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,42 @@
 // enums vs structs, learning about enums and special Option
 #[derive(Debug)]
 
-enum Javascript {
-    TypeScript(bool, String),
-    React(bool),
-    Vue(bool),
-    Svelte(bool),
-    Next(bool),
-    Node(bool, String)
+enum JavascriptTool {
+    Angular(String),
+    React(String),
+    Vue(String),
+    Svelte(String),
+    TypeScript(String),
+    // i see typescript as a tool and not a language, lol typescript,
+    // is written in js for js
+    Grunt(String),
+    Gulp(String),
+    AnyOther(String),
 }
-
-const JS_TYPES: Option<Javascript> = Some(Javascript::React(false));
 
 fn main() {
-    assert_eq!(JS_TYPES.is_none(), false);
+    let tool_info = String::from(
+        "React is extremly user friendly when compared to vue, but vue and svelte are more powerful based on some concepts"
+        );
+
+    let my_best_tool = JavascriptTool::React(tool_info);
+    // since we aren't referencing ownership will be taken
+    alert_tool(my_best_tool);
 }
+
+// i just want to understand how match works
+fn alert_tool(tool: JavascriptTool) -> u8 {
+    match tool {
+        JavascriptTool::React(_) => {
+            println!("yeah React won");
+                0
+        }
+        JavascriptTool::Vue(_) => {
+            println!("Yeah vue is a great choice");
+                1
+        }
+        _=> todo!()
+    }
+}
+
+
