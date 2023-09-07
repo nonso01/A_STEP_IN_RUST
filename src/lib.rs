06466fa366094ddbb977b4878a17158d5ef12157
nonso01 <chukwunonsomartin01@gmail.com>
2023-09-07 21:40:03 +0100

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@
 // going to use `const` to declare global vars.
 
 pub mod what_is_rust {
-    pub fn rust_is() -> &'static str {
+    pub fn rust_is() -> String {
         // haven't covered lifetime yet
-        "Rust is a statically typed language, like C, but sometimes knows what you mean out of the box. Widely used for the creation of a large variety of softwares, ranging from servers , database, embeded sys and AI."
+        "Rust is a statically typed language, like C, but sometimes knows what you mean out of the box. Widely used for the creation of a large variety of softwares, ranging from servers , database, embeded sys and AI.".to_string()
     }
 }
