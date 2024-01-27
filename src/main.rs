1886daacea91ada3eafba593ed9a6b229fde7cd9
nonso01 <chukwunonsomartin01@gmail.com>
2024-01-28 00:26:44 +0100

    using Result enum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use::std::{collections::HashMap, cmp::Ordering, io};
+use::std::{collections::HashMap, cmp::Ordering, io::{self, Read}, fs::File};
 use rand::Rng;
 // Rust is extremly strict, using  /**/ will
 // result as an error, if not used properly.
@@ -8,6 +8,9 @@ const RUST: &str = "this is a constant";
 const BINARY_NUMBER: i8 = 0b110110;
 
 fn main() {
+   let a = read_file_content("hello.txt")
+       .expect("The target file is currently missing!");
+   println!("reading file content.... {:?}", a);
   //  let mut float_x: f32 = 5.5;
 
   //  show_shadowing();
@@ -24,7 +27,11 @@ fn main() {
     
     // println!("{}", t(Some(0)));
     //
-    guessing_game();
+    // guessing_game();
+
+    // let a = quadratic_formula(2.0, 3.0, 1.0);
+    // println!("a is: {:?}", a);
+
 }
 
 fn show_shadowing() {
@@ -109,4 +116,21 @@ fn guessing_game() {
             }
         }
     }
+}
+
+fn quadratic_formula(a: f32, b: f32, c: f32) -> (f32, f32) {
+    // x = (-b ± √b2 - 4ac) / 2a
+    let value_in_square_root: f32 = (b * b) - 4.0 * a * c;
+    let root1: f32 = (-b + value_in_square_root.sqrt()) / (2.0 * a); 
+    let root2: f32 = (-b - value_in_square_root.sqrt()) / (2.0 * a);
+
+    (root1, root2)
+}
+
+fn read_file_content(filename: &str) -> Result<String, io::Error>  {
+let mut _file = File::open(filename)?;
+let mut file_content = String::new();
+
+_file.read_to_string(&mut file_content)?;
+Ok(file_content)
 }
