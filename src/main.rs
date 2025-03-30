6da681425e24397da31f3b7236c3c6294c2acc0b
nonso01 <chukwunonsomartin01@gmail.com>
2025-03-30 11:40:00 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 // use a_step_in_rust::learn_rust::add_binary;
-use std::fs::{self, File};
+use std::fs::{self};
 
 fn main() {
     let a = fs::read_to_string("hello.txt").unwrap();
